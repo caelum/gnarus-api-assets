a8b687c37108ee940f5b0309d559c305c37ff5ef
Bot Commitero <botcommiter@caelum.com.br>
2019-04-02 11:00:19 -0300

    Gerando Share Images [ci skip]

--- bin/generate-png-circleci.sh
+++ bin/generate-png-circleci.sh
@@ -5,14 +5,14 @@ php -S 0.0.0.0:9080 $1.php &
 cd bin
 echo "Gerando png dos icones de curso..."
 node gera-cursos-png.js $1
-echo "Gerando png dos icones de categorias..."
-node gera-categorias-png.js $1
-if [ "${1}" == "alura" ]; then
-	echo "Gerando png dos icones de carreira..."
-	node gera-carreiras-png.js $1
-	echo "Gerando png dos icones de formacao..."
-	node gera-formacoes-png.js $1
-fi
+# echo "Gerando png dos icones de categorias..."
+# node gera-categorias-png.js $1
+# if [ "${1}" == "alura" ]; then
+# 	echo "Gerando png dos icones de carreira..."
+# 	node gera-carreiras-png.js $1
+# 	echo "Gerando png dos icones de formacao..."
+# 	node gera-formacoes-png.js $1
+# fi
 echo "Gerando as imagens de share..."
 node gera-img-share.js $1
 echo "Reduzindo tamanhos dos svgs"
--- bin/generate-png.sh
+++ bin/generate-png.sh
@@ -10,10 +10,10 @@ php -S 0.0.0.0:9080 $1.php &
 cd bin
 echo "Gerando png dos icones de curso..."
 node gera-cursos-png.js $1
-if [ "${1}" == "alura" ]; then
-	echo "Gerando png dos icones de formacao..."
-	node gera-formacoes-png.js $1
-fi	
+# if [ "${1}" == "alura" ]; then
+# 	echo "Gerando png dos icones de formacao..."
+# 	node gera-formacoes-png.js $1
+# fi	
 echo "Gerando as imagens de share..."
 node gera-img-share.js $1
 echo "Reduzindo tamanhos dos svgs"
