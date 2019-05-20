082bd875a9f52e3e53a9fa6a4e521fd07ff0cdb0
Cassio Masteguim <cassio.gm@gmail.com>
2019-05-20 19:13:09 -0300

    testando a aplicação

--- bin/circleci-commit.sh
+++ bin/circleci-commit.sh
@@ -1,4 +1,5 @@
 ./generate-png-circleci.sh alura
+php geradorDeCores.php
 git config --global user.email "botcommiter@caelum.com.br"
 git config --global user.name "Bot Commitero"
 cd ..
@@ -9,7 +10,7 @@ git push -q -u origin ${CIRCLE_BRANCH}
 USERS["claudio"]="O Commiteiro"
 USER_IMGS["claudio"]="\"icon_emoji\":\":cassio_alegre:\""
 
-CHANNEL="#alura_build"
+CHANNEL="#D8GDJH31V"
 MESSAGE="O CircleCI gerou as share-images na branch ${CIRCLE_BRANCH}. Veja aqui https://github.com/caelum/alura-site/commit/$(git log -n 1 --pretty=format:'%H')"
 
 DATA="payload={\"type\":\"message\",\"text\":\"$MESSAGE\",\"thread_ts\":\"1489097792.053209\",\"channel\":\"$CHANNEL\",\"username\":\"${USERS[$USERNAME]}\",${USER_IMGS[$USERNAME]}}"
