d7214869eabc0d05c133bc2ea5d7e277a31fed63
carlosmuller <kdu428@gmail.com>
2022-06-07 13:44:27 -0300

    chore(jenkins): começando o jenkinsfile, falta o node

---
+++ bin/generate-assets.sh
@@ -0,0 +1,15 @@
+set -e
+./generate-png-circleci.sh alura
+php geradorDeCores.php -i alura
+./generate-png-circleci.sh musicdot
+php geradorDeCores.php -i musicdot
+./generate-png-circleci.sh alurastart
+php geradorDeCores.php -i alurastart
+./generate-png-circleci.sh aluralingua
+php geradorDeCores.php -i aluralingua
+
+cd ..
+git remote
+git add .
+git commit -m "Gerando Share Images [ci skip]"
+git push -q -u origin ${BRANCH_NAME}
