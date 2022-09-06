c2ee79bb403e3fd17bea34a680a9285a1fac8683
Jonilson Sousa <jonilsonds9@gmail.com>
2022-09-06 10:50:48 -0300

    [jenkins] deixando mais rapido os testes

--- bin/generate-assets.sh
+++ bin/generate-assets.sh
@@ -1,12 +1,12 @@
 set -e
-./generate-png-circleci.sh alura
-php geradorDeCores.php -i alura
-./generate-png-circleci.sh musicdot
-php geradorDeCores.php -i musicdot
+#./generate-png-circleci.sh alura
+#php geradorDeCores.php -i alura
+#./generate-png-circleci.sh musicdot
+#php geradorDeCores.php -i musicdot
 ./generate-png-circleci.sh alurastart
 php geradorDeCores.php -i alurastart
-./generate-png-circleci.sh aluralingua
-php geradorDeCores.php -i aluralingua
+#./generate-png-circleci.sh aluralingua
+#php geradorDeCores.php -i aluralingua
 
 #cd ..
 #git remote
