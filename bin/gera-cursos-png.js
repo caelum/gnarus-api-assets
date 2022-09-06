bb151257f89a92dff0d07951ca1f07bfeaed9883
Jonilson Sousa <jonilsonds9@gmail.com>
2022-09-06 14:11:30 -0300

    [jenkins] tentando ver phantom

--- bin/gera-cursos-png.js
+++ bin/gera-cursos-png.js
@@ -3,12 +3,34 @@ var env = process.argv[2];
 
 var geradorDeGeraProxima = require('./gerador');
 var coletorDeSlugsLocal = require('./coletor-de-slugs-local');
-var phantom = require('phantomjs-prebuilt');
-console.log('using PhantomJS version ' +
-    phantom.version.major + '.' +
-    phantom.version.minor + '.' +
-    phantom.version.patch);
-phantom.exit();
+
+var spawn = require("child_process").spawn
+var execFile = require("child_process").execFile
+
+var child = spawn("ls", ["-lF", "/rooot"])
+
+child.stdout.on("data", function (data) {
+    console.log("spawnSTDOUT:", JSON.stringify(data))
+})
+
+child.stderr.on("data", function (data) {
+    console.log("spawnSTDERR:", JSON.stringify(data))
+})
+
+child.on("exit", function (code) {
+    console.log("spawnEXIT:", code)
+})
+
+//child.kill("SIGKILL")
+
+execFile("ls", ["-lF", "/usr"], null, function (err, stdout, stderr) {
+    console.log("execFileSTDOUT:", JSON.stringify(stdout))
+    console.log("execFileSTDERR:", JSON.stringify(stderr))
+})
+
+setTimeout(function () {
+    phantom.exit(0)
+}, 2000)
 
 // config
 var url =  'http://0.0.0.0:9080/'+env+'/assets/api/cursos/';
